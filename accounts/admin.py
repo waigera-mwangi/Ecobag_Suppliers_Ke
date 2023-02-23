a4291bc38b65cc3177704d478652560a9166fb60
waigera-mwangi <waigeram019@gmail.com>
2023-02-23 05:44:07 +0100

    updated user registration form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,74 @@
-from django.contrib import admin
-from .models import User
-from .models import Profile
+from django.contrib import admin, messages
+from .models import User, Profile
+from accounts.forms import UserAdminChangeForm, RegistrationForm
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ngettext
 
-admin.site.site_header = "Ecobag Suppliers ke, ADMIN "
-admin.site.site_title = "Ecobag Suppliers ke "
-admin.site.index_title = "Ecobag Suppliers ke"
-admin.site.register(User)
 
+admin.site.site_header = "Ecobag Suppliers ke Admin"
+admin.site.site_title = "Ecobag Suppliers ke Admin Portal"
+admin.site.index_title = "Welcome to Ecobag Suppliers ke Management portal"
+
+admin.site.unregister(Group)
+# admin.site.register(Profile)
 # class ProfileAdmin(admin.ModelAdmin):
 #     list_display('name','image', 'gender')
 
-admin.site.register(Profile)
+# override password
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = RegistrationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('first_name', 'last_name', 'email', 'username', 'user_type')
+    search_fields = ('first_name', 'last_name', 'email', 'username',)
+    list_filter = ('is_active', 'user_type')
+
+    fieldsets = (
+        (None, {'fields': ('password',)}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'username')}),
+        ('Permissions', {'fields': ('user_type',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'username', 'user_type', 'password1', 'password2')}
+         ),
+    )
+    ordering = ['email']
+    filter_horizontal = ()
+
+    @staticmethod
+    def has_delete_permission(request, obj=None):
+        return False
+
+    @staticmethod
+    def has_change_permission(request, obj=None):
+        return True
+
+    @staticmethod
+    def has_add_permission(request, obj=None):
+        return True
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(ModelAdmin):
+#     search_fields = ['user__username']
+#     list_display = ['user', 'phone_number', 'image']
+#     list_display_links = ['user', ]
+#     search_help_text = "Search by username"
+#     list_filter = ('updated', 'created')
+
+#     @staticmethod
+#     def has_change_permission(request, obj=None):
+#         return True
 
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm,ReadOnlyPasswordHashField
 from django.contrib.auth import get_user_model, logout
 from django.forms import forms, ModelForm
 from django import forms
-
+from accounts.models import Profile
 from accounts.models import CustomerProfile, Customer
 
 User = get_user_model()
@@ -151,3 +151,34 @@ class CustomerForm(ModelForm):
         model = Customer
         fields = ['last_name', 'first_name', 'email']
 
+# overiding password
+class RegistrationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'user_type', 'password1', 'password2']
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        return data.upper()
+
+class UserAdminChangeForm(ModelForm):
+    """
+    A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admins
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password', 'is_active']
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        return data.upper()
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
