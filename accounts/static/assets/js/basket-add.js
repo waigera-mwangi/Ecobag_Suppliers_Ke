5a299a39c0b7d19fa12bd2c3b517e46144e889af
waigera-mwangi <waigeram019@gmail.com>
2023-03-12 17:36:09 +0300

    secure https

---
+++ accounts/static/assets/js/basket-add.js
@@ -0,0 +1,20 @@
+
+	$(document).on('click','#add-button', function(e){
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: '{% url "basket:basket_add" %}',
+			data: {
+				productid: $('#add-button').val(),
+				productqty: $('select option:selected').text(),
+				csrfmiddlewaretoken: '{{csrf_token}}',
+				action: 'post'
+			},
+			success: function(json){
+				document.getElementById("basket-qty").innerHTML = json.qty
+			},
+			error: function (xhr, errmsg, err){
+
+			}
+		});
+	})
