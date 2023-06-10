edeb5697db0735b99865e2b784ecb950024fdaf9
waigera-mwangi <waigeram019@gmail.com>
2023-06-10 13:02:11 +0300

    here

--- supply/views.py
+++ supply/views.py
@@ -313,7 +313,7 @@ class InventoryConfirmTenderListView(ListView):
     context_object_name = 'tenders'
 
     def get_queryset(self):
-        SupplyTender.objects.filter(tender_status__icontains='complete') | SupplyTender.objects.filter(tender_status__icontains='confirmed')
+        SupplyTender.objects.filter(tender_status__icontains='Complete') | SupplyTender.objects.filter(tender_status__icontains='Confirmed')
 
 
 class FinanceSupplierTenderListView(ListView):
