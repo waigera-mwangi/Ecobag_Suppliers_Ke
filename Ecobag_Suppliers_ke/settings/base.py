a13ca5c4a2908b7778d05cd7c0beadefe9b70503
waigera-mwangi <waigeram019@gmail.com>
2023-06-12 16:55:17 +0300

    here

--- Ecobag_Suppliers_ke/settings/base.py
+++ Ecobag_Suppliers_ke/settings/base.py
@@ -136,11 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 # SMTP configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'waigeram019@gmail.com'
-# EMAIL_HOST_PASSWORD = '###@@@###'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'waigeram019@gmail.com'
+DEFAULT_FROM_EMAIL = 'waigeram019@gmail.com'
+EMAIL_HOST_PASSWORD = '###@@@###'
 
 STRIPE_ENDPOINT_SECRET = 'whsec_myIuLuukACB4Mo9HQH)ZyqgOeEc4yYov'
