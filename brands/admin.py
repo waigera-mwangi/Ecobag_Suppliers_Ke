631f6a1bb34d89faaa056836f435314ee8687e64
waigera-mwangi <waigeram019@gmail.com>
2023-07-12 17:55:39 +0300

    here

--- brands/admin.py
+++ brands/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Brand
 
-admin.site.register(Brand)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('order_tno','user','brand_name', 'created')
+    list_filter = ('created','user','brandstatus')
+    search_fields = ('id','user_username')
+    readonly_fields = ('created', 'brand_logo')
+    
+    def order_tno(self, obj):
+        return obj.brand.order_tno
+
+admin.site.register(Brand, BrandAdmin)
