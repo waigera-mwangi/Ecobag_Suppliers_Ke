55b4908fa63b8e39f837e77d2edfbcb22549c417
waigera-mwangi <waigeram019@gmail.com>
2023-07-29 13:20:41 +0300

    here

--- brands/admin.py
+++ brands/admin.py
@@ -4,12 +4,21 @@ from .models import Brand
 
 
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('order_tno','user','brand_name', 'created')
+    list_display = ('order_tno','user','brand_name', 'created',)
     list_filter = ('created','user','brandstatus')
     search_fields = ('id','user_username')
-    readonly_fields = ('created', 'brand_logo')
+    # readonly_fields = ('created', 'brand_logo')
     
     def order_tno(self, obj):
         return obj.brand.order_tno
+    
+    def download_logo(self, obj):
+        if obj.brand_logo:
+            return '<a href="{}" download>Download</a>'.format(obj.brand_logo.url)
+        return 'No logo available'
+
+    download_logo.allow_tags = True
+    download_logo.short_description = 'Logo Download'
+    
 
 admin.site.register(Brand, BrandAdmin)
